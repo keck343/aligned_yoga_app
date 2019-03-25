3ed1e7f22487f0cd3e29b52088647a182131138d
Quinn Keck <akeck@dons.usfca.edu>
2019-03-24 19:30:33 -0700

    remove q-test

--- convert_save_avi.sh
+++ convert_save_avi.sh
@@ -5,6 +5,6 @@ for f in *.mov
 do
 ffmpeg -i $f -acodec copy -vcodec copy ${f/.mov/.avi}
 echo "Converted file $f"
-aws s3 cp ${f/.mov/.avi} s3://alignedstorage/videos/q-test --acl bucket-owner-full-control
+aws s3 cp ${f/.mov/.avi} s3://alignedstorage/videos/ --acl bucket-owner-full-control
 echo "Saved file $f to s3"
 done
