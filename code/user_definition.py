3dba0c87702a28fe8cfa586c78f710051bae3f80
bdorsey2 <40878527+bdorsey2@users.noreply.github.com>
2019-03-26 20:41:52 -0700

    HW1 commit

---
+++ code/user_definition.py
@@ -0,0 +1,9 @@
+ec2_address = "ec2-54-149-49-78.us-west-2.compute.amazonaws.com"
+user = "ec2-user"
+key_file = "\\briand.pem"
+git_repo_owner = "MSDS698"
+git_repo_name = "msds_603_example"
+git_user_id = "bdorsey2"
+orig_coord = '37.7909,-122.3925'
+dest_coord = '37.7765,-122.4506'
+output_file_name = 'output.txt'
