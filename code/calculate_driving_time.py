90c7b8e16d18efc8783df65921238ea88c590bbc
bdorsey2 <40878527+bdorsey2@users.noreply.github.com>
2019-03-26 21:59:41 -0700

    added correct apikey

--- code/calculate_driving_time.py
+++ code/calculate_driving_time.py
@@ -6,7 +6,7 @@ from user_definition import *
 
 output_file = open(git_repo_name+"/"+output_file_name, "a")
 # https://developers.google.com/maps/documentation/javascript/get-api-key
-apikey = 'AIzaSyCTWPyWsLgoMH6w_1iEf161paxhTiK1dNk'
+apikey = 'AIzaSyAmeBuOF4O11_A_j_WxVXCD5yQYn0--Veg'
 url = "https://maps.googleapis.com/maps/api/distancematrix/json?" \
       "key={0}&origins={1}&destinations={2}&mode=driving&departure_time=now" \
       "&language=en-EN&sensor=false".\
