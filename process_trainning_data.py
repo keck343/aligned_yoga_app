9a7779a8e361cb8e705539785c0938e9666f3a43
Quinn Keck <akeck@dons.usfca.edu>
2019-04-04 22:31:43 -0700

    fix indent 2

--- process_trainning_data.py
+++ process_trainning_data.py
@@ -18,41 +18,41 @@ bucket = boto3.resource('s3').Bucket(bucket_name)
 # loops through all files in the bucket
 for obj in bucket.objects.filter(Prefix="training_input/"):
     path, file = os.path.split(obj.key)
-
+    if len(file_name) > 0:  # "" file names
     # 1. grab name of the file
-    file_name = str(file)
-    print("file name:", file_name)
-    if len(file_name) > 0:
-   	 name, _ = os.path.splitext(file_name)
-   	 # 2. save file in tmp
-   	 file_path = "/tmp/" + file_name
-   	 bucket.download_file("training_input/"+file_name, file_path)
-   	 # 3. create folder for json
-   	 output_dir = "/tmp/json_data"  # without extension
-   	 if os.path.isdir(output_dir) == False:
-        	os.mkdir(output_dir)
-   	 s3_dir = "training_data/" + name + "/"
-   	 processed_path = "/tmp/" + name + "_processed.avi"
-   	 openpose_path = "/home/ubuntu/openpose/build/examples/openpose/openpose.bin"
+        file_name = str(file)
+        print("file name:", file_name)
+        name, _ = os.path.splitext(file_name)
+        print(name)
+   	# 2. save file in tmp
+        file_path = "/tmp/" + file_name
+        bucket.download_file("training_input/"+file_name, file_path)
+   	# 3. create folder for json
+       	 output_dir = "/tmp/json_data"  # without extension
+       	 if os.path.isdir(output_dir) == False:
+             os.mkdir(output_dir)
+        s3_dir = "training_data/" + name + "/"
+   	    processed_path = "/tmp/" + name + "_processed.avi"
+   	    openpose_path = "/home/ubuntu/openpose/build/examples/openpose/openpose.bin"
 
-   	 # 4. Run openpose
-   	 openpose_cmd = [
+   	# 4. Run openpose
+        openpose_cmd = [
         	openpose_path,
        		 "--video",
         	file_path,
        		 "--write_video",
         	processed_path,
         	"--write_json",
-		output_dir,
+		    output_dir,
         	"--display",
         	"0"]
 
     # process = sh.swfdump(_ for _ in openpose_cmd)
-	 process = subprocess.Popen(openpose_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-   	 stdout, stderr = process.communicate()
+        process = subprocess.Popen(openpose_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+       	 stdout, stderr = process.communicate()
          print(stderr)  # Print potential error
 
     # 5. save output to s3 and delete locally
-   	 upload_and_delete(output_dir, s3_dir)
+       	 upload_and_delete(output_dir, s3_dir)
     	 #os.remove(file_path)
     	 #os.remove(processed_path)
