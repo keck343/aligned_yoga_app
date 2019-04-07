81b54c57ed7a00f88a9f03286fab7c30ba399727
Connor Swanson <cswanson@dons.usfca.edu>
2019-04-07 15:29:54 -0700

    ssh

--- front_end_server/code/app/routes_2.py
+++ front_end_server/code/app/routes_2.py
@@ -20,7 +20,8 @@ def send_file():
 def open_pose():
     #ssh into OpenPose server
     ec2_address = 'ec2-54-193-96-157.us-west-1.compute.amazonaws.com'
-    k = paramiko.RSAKey.from_private_key_file("/Users/connorswanson/desktop/credentials/aligned.pem")
+    #k = paramiko.RSAKey.from_private_key_file("/Users/connorswanson/desktop/credentials/aligned.pem")
+    k = paramiko.RSAKey.from_private_key_file("/home/ubuntu/front_end_server/code/app/aligned.pem")
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     ssh.connect(ec2_address, username='ubuntu', pkey=k)
