178329a166626a2b8e6aabd4ac94ac9a75afef23
Evan Calkins <emcalkins@usfca.edu>
2019-04-11 16:47:46 -0700

    indent mod

--- process_openpose_user.py
+++ process_openpose_user.py
@@ -9,14 +9,14 @@ import sys
 
 
 def df2csv_s3(df, s3_path, bucket_name='alignedstorage'):
-	"""
-	Convert Pandas dataframe to csv and upload to s3.
-	"""
-	s3 = boto3.resource('s3')
-	bucket = s3.Bucket(bucket_name)
-	csv_buffer = StringIO()
-	df.to_csv(csv_buffer)
-	bucket.put_object(Key=s3_path, Body=csv_buffer.getvalue(), ACL='public-read')
+    """
+    Convert Pandas dataframe to csv and upload to s3.
+    """
+    s3 = boto3.resource('s3')
+    bucket = s3.Bucket(bucket_name)
+    csv_buffer = StringIO()
+    df.to_csv(csv_buffer)
+    bucket.put_object(Key=s3_path, Body=csv_buffer.getvalue(), ACL='public-read')
 
 def upload_and_delete(local_dir, s3_path):
     """
@@ -27,15 +27,15 @@ def upload_and_delete(local_dir, s3_path):
     for subdir, dirs, files in os.walk(local_dir):
         for i, file in enumerate(files):
             full_path = os.path.join(subdir, file)
-			try:
+            try:
                 with open(full_path, 'rb') as f:
                     json_file = json.load(f)
                 data = json_file['people'][0]['pose_keypoints_2d']
                 df.loc[i] = data
             except:
                 continue
-		df2csv_s3(df=df, s3_path=s3_path)
-	shutil.rmtree(subdir)   # delete directory and contents
+        df2csv_s3(df=df, s3_path=s3_path)
+    shutil.rmtree(subdir)   # delete directory and contents
 
 
 path_video = sys.argv[1]
@@ -51,25 +51,26 @@ processed_path = "/tmp/" + name + "_processed.avi"
 openpose_path = "/home/ubuntu/openpose/build/examples/openpose/openpose.bin"
 
 # Download file from bucket
-bucket_name = 'alignedstorage'
+#bucket_name = 'alignedstorage'
+bucket_name = 'aligned2'
 bucket = boto3.resource('s3').Bucket(bucket_name)
 bucket.download_file(path_video, path_local)
 
 # Create output directory if necessary
 if os.path.isdir(output_dir) == False:
-	os.mkdir(output_dir)
+    os.mkdir(output_dir)
 
 # Run openpose on downloaded video
 openpose_cmd = [
-	openpose_path,
-	"--video",
-	file_path,
-	"--write_video",
-	processed_path,
-	"--write_json",
-	output_dir,
-	"--display",
-	"0"]
+    openpose_path,
+    "--video",
+    file_path,
+    "--write_video",
+    processed_path,
+    "--write_json",
+    output_dir,
+    "--display",
+    "0"]
 
 process = subprocess.Popen(openpose_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 stdout, stderr = process.communicate()
