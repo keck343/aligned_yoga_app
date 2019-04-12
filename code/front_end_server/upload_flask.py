effdce130d70c24bb79f0c1a88f60a95ffafe7f0
skhaniyur <skhaniyur@gmail.com>
2019-04-12 10:35:39 -0700

    edit flask route

--- code/front_end_server/upload_flask.py
+++ code/front_end_server/upload_flask.py
@@ -113,7 +113,7 @@ def register():
     user = UserFileForm() # UserFileForm class instance
     # Check if it is a POST request and if it is valid.
     if user.validate_on_submit():
-        filename = user.first_name.label
+        filename = user.first_name
         return redirect(url_for('upload', fname=filename))
 
     return render_template('register.html', form=user)
