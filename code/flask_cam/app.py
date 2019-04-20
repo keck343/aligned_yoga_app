66d2542590731125b8c259a06212b9b33c050691
bdorsey2 <40878527+bdorsey2@users.noreply.github.com>
2019-04-20 12:49:01 -0700

    integrating video code with prod code

--- code/flask_cam/app.py
+++ code/flask_cam/app.py
@@ -16,33 +16,16 @@ def index():
 @app.route('/video', methods=['GET', 'POST'])
 def video():
     if request.method == 'POST':
-        for k in request.files.keys():
-            print(k)
-
         file = request.files['file']
         
-        filename = file.filename
+        filename = secure_filename(file.filename)
+        print(type(file))
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
         ff = ffmpy.FFmpeg(inputs={'video.webm' : None},
                           outputs={'outputs.avi' : '-q:v 0'})
         ff.run()
-        return redirect(url_for('index'))
+        return url_for('index')
     return render_template('video.html')
-
-
-def gen():
-    """Video streaming generator function."""
-    app.logger.info("starting to generate frames!")
-    while True:
-        frame = camera.get_frame() #pil_image_to_base64(camera.get_frame())
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-
-
-@app.route('/video_feed')
-def video_feed():
-    """Video streaming route. Put this in the src attribute of an img tag."""
-    return Response(gen(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 if __name__ == '__main__':
